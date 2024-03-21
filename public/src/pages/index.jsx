5cee332ca553bc945bc3c0cb6bab07b613de1418
AbbieWills <119050905+AbbieWills@users.noreply.github.com>
2024-03-21 13:13:53 +0000

    route

--- public/src/pages/index.jsx
+++ public/src/pages/index.jsx
@@ -2,6 +2,6 @@ export {default as Home} from "./Home"
 export {default as About} from "./About"
 export {default as Contact} from "./Contact"
 export {default as MeetMaker} from "./MeetMaker"
-export {default as NotFound} from "./NotFound"
+export {default as NotFound} from "./NotFound.jsx"
 
 
