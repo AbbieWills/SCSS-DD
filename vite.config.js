4b8e85663345868f9b17e8d9c7e1176495ea54d6
AbbieWills <119050905+AbbieWills@users.noreply.github.com>
2025-05-19 15:25:35 +0100

    attempt to fix 404 error

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: 'jsdom'
+  },
+
+  server: {
+    historyApiFallback: true,
   }
 })
